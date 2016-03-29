99fa44009b305dae54a2a05f5ef569b0038ea1fd
Jason Dujardin-Terry <jason.dujardin@accedo.tv>
2016-03-29 16:21:37 +0200

    Killed a hanging comma

--- npm-module/rollup.config.es6.js
+++ npm-module/rollup.config.es6.js
@@ -2,7 +2,7 @@ import baseConfig from './rollup.config';
 
 const additionalConfig = {
   format: 'es6',
-  dest: 'dist/bundle.es6.js',
+  dest: 'dist/bundle.es6.js'
 };
 
 const config = Object.assign({}, baseConfig, additionalConfig);
