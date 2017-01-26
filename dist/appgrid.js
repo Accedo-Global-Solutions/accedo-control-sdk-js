da067879dfe06ec3dc549017a5b4a5aec04acb93
Gregory Desfour <gregory.desfour@accedo.tv>
2017-01-26 16:59:32 +0100

    :sparkles: Add browser persistence strategy

--- dist/appgrid.js
+++ dist/appgrid.js
@@ -597,6 +597,10 @@ const stamp = stampit().compose(
 
 const noop = () => {};
 
+const hasLocalStorage = (typeof localStorage !== 'undefined');
+const hasSessionStorage = (typeof sessionStorage !== 'undefined');
+const hasSomeWebStorage = hasLocalStorage || hasSessionStorage;
+
 /**
  * Check the parameters given are good enough to make api calls
  * @function
@@ -662,6 +666,82 @@ const appgrid = (config) => {
   });
 };
 
+const WEBSTORAGE_DEVICE_ID = 'ag_d';
+const WEBSTORAGE_SESSION_KEY = 'ag_s';
+
+/**
+ * Default implementation for the browserInfoProvider option (applied to browsers only).
+ * This will persist deviceId in localStorage and sessionKey in sessionStorage.
+ *
+ * @private
+ * @return {Object} An object with both deviceId and sessionKey
+ */
+const defaultBrowserInfoProvider = () => {
+  // Take deviceId from localStorage
+  const deviceId = localStorage[WEBSTORAGE_DEVICE_ID];
+
+  // Take sessionKey from sessionStorage
+  const sessionKey = (hasSessionStorage) ? sessionStorage[WEBSTORAGE_SESSION_KEY] : undefined;
+
+  return { deviceId, sessionKey };
+};
+
+const defaultBrowserOnDeviceIdGenerated = (id) => {
+  if (!hasLocalStorage) { return; }
+
+  localStorage[WEBSTORAGE_DEVICE_ID] = id;
+};
+
+const defaultBrowserOnSessionKeyChanged = (key) => {
+  if (!hasSessionStorage) { return; }
+
+  sessionStorage[WEBSTORAGE_SESSION_KEY] = key;
+};
+
+/**
+ * A wrapper over the appgrid factory for clients that support Web Storage by default
+ * (most browsers). Provides default `onDeviceIdGenerated` and `onSessionKeyChanged` callbacks for
+ * persistency through Web Storage. Also sets the `deviceId` and `sessionKey` values from
+ * Web Storage to the factory when not given as an option.
+ *
+ * This behaviour can be customised by providing a `browserInfoProvider` function returning an
+ * object with the keys `deviceId` and `sessionKey`. You could implement a different strategy
+ * relying on cookies or a device-specific API for instance.
+ *
+ * If you do, you will want to customise the `onSessionKeyChanged` and `onDeviceIdGenerated`
+ * callbacks as well.
+ *
+ * Refer to the factory's documentation to see other available options.
+ *
+ * Note this is this library's default export for Web Storage-enabled devices.
+ *
+ * @function
+ * @param  {object} config the configuration for the new instance
+ * @param  {string} config.browserInfoProvider A function to customise the persistency strategy
+ * @param  {any} [config.appKey/log/gid/etc] You should also pass any extra option accepted by the appgrid factory function (appKey, log, gid, ...)
+ * @return {client}        an AppGrid client tied to the given params
+ */
+const appgridWrapperForBrowsers = (config) => {
+  if (!hasSomeWebStorage) {
+    return appgrid(config);
+  }
+
+  const {
+    browserInfoProvider = defaultBrowserInfoProvider,
+    onDeviceIdGenerated = defaultBrowserOnDeviceIdGenerated,
+    onSessionKeyChanged = defaultBrowserOnSessionKeyChanged
+  } = config;
+  let { deviceId, sessionKey } = config;
+
+  if ((!deviceId || !sessionKey) && browserInfoProvider) {
+    const browserInfo = browserInfoProvider();
+    deviceId = deviceId || browserInfo.deviceId;
+    sessionKey = sessionKey || browserInfo.sessionKey;
+  }
+
+  return appgrid(Object.assign({}, config, { deviceId, sessionKey, onDeviceIdGenerated, onSessionKeyChanged }));
+};
+
 /**
  * Generate a UUID.
  *
@@ -673,4 +753,6 @@ const appgrid = (config) => {
  */
 appgrid.generateUuid = () => uuidLib.v4();
 
-module.exports = appgrid;
+var index = hasSomeWebStorage ? appgridWrapperForBrowsers : appgrid;
+
+module.exports = index;
