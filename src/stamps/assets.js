66dc69049f73583301a48ab4d90b8ca5daa8eef7
Gregory Desfour <gregory.desfour@accedo.tv>
2017-05-11 12:16:49 +0800

    :memo: Remove getAssetById from the docs, better than leaving it there marked as deprecated.

--- src/stamps/assets.js
+++ src/stamps/assets.js
@@ -12,12 +12,7 @@ const stamp = stampit()
     return this.withSessionHandling(() => grab('/asset', this.props.config));
   },
 
-  /**
-   * Get a stream for one asset by id. DEPRECATED, DO NOT USE ! Always rely on getAllAssets instead.
-   * @deprecated
-   * @param {string} id the asset id
-   * @return {promise}  a promise of a node stream
-   */
+  // DEPRECATED, DO NOT USE ! Always rely on getAllAssets instead.
   getAssetById(id) {
     // note this method does not need a session
     return grabRaw(`/asset/file/${id}`, this.props.config);
